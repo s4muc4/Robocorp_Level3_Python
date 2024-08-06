e8191ba197eeac32d91188383b3983650a693351
s4muc4 <eng.comp.samuel@hotmail.com>
2024-08-06 00:20:03 -0300

    feature: adding payload item as work-item

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 from robocorp.tasks import task
+from robocorp import workitems
 from RPA.HTTP import HTTP
 from RPA.JSON import JSON
 from RPA.Tables import Tables
@@ -9,6 +10,12 @@ json = JSON()
 table = Tables()
 
 TRAFFIC_JSON_FILE_PATH = "output/traffic.json"
+
+# JSON data keys
+COUNTRY_KEY = "SpatialDim"
+YEAR_KEY = "TimeDim"
+RATE_KEY = "NumericValue"
+GENDER_KEY = "Dim1"
 
 @task
 def produce_traffic_data():
@@ -22,6 +29,7 @@ def produce_traffic_data():
     filtered_data = filter_and_sort_traffic_data(traffic_data)
     filtered_data = get_latest_data_by_country(filtered_data)
     payloads = create_work_item_payloads(filtered_data)
+    save_work_item_payloads(payloads)
 
 @task
 def consume_traffic_data():
@@ -32,20 +40,18 @@ def load_traffic_data_as_table():
     return table.create_table(json_data["value"])
 
 def filter_and_sort_traffic_data(data):
-    rate_key = "NumericValue"
     max_rate = 5.0
-    gender_key = "Dim1"
     both_genders = "BTSX"
-    year_key = "TimeDim"
+    
 
-    table.filter_table_by_column(data, rate_key, "<", max_rate)
-    table.filter_table_by_column(data, gender_key, "==", both_genders)
-    table.sort_table_by_column(data, year_key, False)
+    table.filter_table_by_column(data, RATE_KEY, "<", max_rate)
+    table.filter_table_by_column(data, GENDER_KEY, "==", both_genders)
+    table.sort_table_by_column(data, YEAR_KEY, False)
     return data
 
 def get_latest_data_by_country(data):
-    contry_key = "SpatialDim"
-    data = table.group_table_by_column(data, contry_key)
+    
+    data = table.group_table_by_column(data, COUNTRY_KEY)
     latest_data_by_contry = []
     for group in data:
         first_row = table.pop_table_row(group)
@@ -54,4 +60,17 @@ def get_latest_data_by_country(data):
     return latest_data_by_contry
 
 def create_work_item_payloads(traffic_data):
-    print()
+    payloads = []
+    for row in traffic_data:
+        payload = dict(
+            contry = row[COUNTRY_KEY],
+            year = row[YEAR_KEY],
+            rate = row[RATE_KEY]
+        )
+        payloads.append(payload)
+    return payloads
+
+def save_work_item_payloads(payloads):
+    for payload in payloads:
+        variables = dict(traffic_data=payload)
+        workitems.outputs.create(variables)
